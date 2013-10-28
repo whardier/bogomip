1babc4b6d31e88de5cc4b5f360372e409a26dc23
Shane R. Spencer <sspencer@abrinc.com>
2013-10-28 12:19:44 -0800

    test

--- conf.py
+++ conf.py
@@ -13,7 +13,7 @@
 
 import sys, os
 
-import sphinx_bootstrap_theme
+sys.path.append(os.path.abspath('_themes'))
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -93,21 +93,15 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'bootstrap'
+html_theme = 'kr'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #html_theme_options = {}
 
-html_theme_options = {
-    'navbar_title': "bogomip",
-    'bootswatch_theme': "spacelab",
-    'source_link_position': "footer",
-}
-
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+html_theme_path = ['_themes']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
