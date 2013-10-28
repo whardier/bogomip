6ea93b96d5348e811e207be1c56971ced68f0b57
Shane R. Spencer <sspencer@abrinc.com>
2013-10-28 12:34:03 -0800

    submodule

--- conf.py
+++ conf.py
@@ -13,8 +13,6 @@
 
 import sys, os
 
-sys.path.append(os.path.abspath('_themes'))
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -93,7 +91,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'kr'
+html_theme = 'nature'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -101,7 +99,7 @@ html_theme = 'kr'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ['_themes']
+#html_theme_path = []
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
